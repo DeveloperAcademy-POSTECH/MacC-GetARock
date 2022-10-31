daf31759cc8b34e5c26c863267ec9c2e59a24364
skycat0212 <mrmagnet777@gmail.com>
2022-11-01 08:10:12 +0900

    [Chore] 세부 폴더 생성 (#9)

---
+++ GetARock/GetARock/Global/UIComponent/SampleView.swift
@@ -0,0 +1,20 @@
+//
+//  SampleView.swift
+//  GetARock
+//
+//  Created by 김수진 on 2022/11/01.
+//
+
+import UIKit
+
+class SampleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
