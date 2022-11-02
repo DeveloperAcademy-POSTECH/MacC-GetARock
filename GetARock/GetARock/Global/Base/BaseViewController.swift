2d6081a619476088e388224e091e9fa2399f433a
skycat0212 <mrmagnet777@gmail.com>
2022-11-02 17:54:48 +0900

    [Add] 불필요한 공백문자 삭제 (#13)

--- GetARock/GetARock/Global/Base/BaseViewController.swift
+++ GetARock/GetARock/Global/Base/BaseViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class BaseViewController: UIViewController {
 
-    override func viewDidLoad  () {
+    override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
