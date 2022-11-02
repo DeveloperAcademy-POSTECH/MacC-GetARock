db493a850c0d807a4115fe77e4e175cb31b4b38f
skycat0212 <mrmagnet777@gmail.com>
2022-11-02 16:50:47 +0900

    [Add] lint 적용 (#13)

--- GetARock/GetARock/Global/Base/BaseViewController.swift
+++ GetARock/GetARock/Global/Base/BaseViewController.swift
@@ -9,13 +9,12 @@ import UIKit
 
 class BaseViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad  () {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
     /*
     // MARK: - Navigation
 
