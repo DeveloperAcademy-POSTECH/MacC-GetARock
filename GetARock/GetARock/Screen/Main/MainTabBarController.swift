84a49ea1d22c431ed582079e713da86bd521ed49
skycat0212 <mrmagnet777@gmail.com>
2022-11-07 03:38:44 +0900

    [Design] tabBar 세팅하기 (#28)

--- GetARock/GetARock/Screen/Main/MainTabBarController.swift
+++ GetARock/GetARock/Screen/Main/MainTabBarController.swift
@@ -8,20 +8,74 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
+    
+    enum Mode {
+        case individual
+        case team
+        
+        var recomendedTitle: String {
+            switch self {
+            case .individual: return "밴드 추천"
+            case .team: return "멤버 추천"
+            }
+        }
+    }
 
     // MARK: - Property
+    var mode: Mode = .individual
     
     // MARK: - View
+    lazy var recomendedListViewController: UIViewController = {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .orange
+        viewController.tabBarItem = UITabBarItem(title: mode.recomendedTitle,
+                                                 image: UIImage(systemName: "list.bullet"),
+                                                 tag: 0)
+        return viewController
+    }()
+    let myPageViewController: UIViewController = {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .yellow
+        viewController.tabBarItem = UITabBarItem(title: "마이페이지",
+                                                 image: UIImage(systemName: "person"),
+                                                 tag: 1)
+        return viewController
+    }()
     
     // MARK: - Life Cycle
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setTabBar()
     }
     
     // MARK: - Method
+    func setTabBar() {
+        setAppearance()
+        setViewControllers([recomendedListViewController, myPageViewController],
+                           animated: false)
+    }
+    
+    func setAppearance() {
+        // TODO: color 값 업데이트 되면 색상 세팅 다시하기
+        tabBar.isTranslucent = false
+        tabBar.tintColor = .systemPurple
+        tabBar.unselectedItemTintColor = .lightGray
+        
+        let tabBarAppearance = UITabBarAppearance()
+        
+        tabBarAppearance.backgroundColor = .darkGray
+        tabBarAppearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.systemPurple]
+        tabBarAppearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.systemGray3]
+        tabBarAppearance.stackedLayoutAppearance.normal.iconColor = UIColor.systemGray3
+        tabBarAppearance.stackedLayoutAppearance.selected.iconColor = UIColor.systemPurple
 
+        tabBar.standardAppearance = tabBarAppearance
+        if #available(iOS 15.0, *) {
+            tabBar.scrollEdgeAppearance = tabBarAppearance
+        }
+    }
+    
 }
 
 // MARK: - use Canvas
