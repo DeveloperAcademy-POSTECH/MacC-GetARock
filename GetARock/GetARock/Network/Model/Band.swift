3ba57532a6be87e5729da1533a3e31652c58d170
skycat0212 <mrmagnet777@gmail.com>
2022-11-18 00:13:33 +0900

    [Add] 연령대 정보 정의 (#35)

--- GetARock/GetARock/Network/Model/Band.swift
+++ GetARock/GetARock/Network/Model/Band.swift
@@ -29,3 +29,25 @@ enum PlayPosition {
         }
     }
 }
+
+enum AgeGroup {
+    case lessThanTwenty
+    case twenties
+    case thirties
+    case forties
+    case fifties
+    case overThanSixty
+    
+    func toKorean() -> String {
+        switch self {
+        case .lessThanTwenty: return "20대 미만"
+        case .twenties: return "20대"
+        case .thirties: return "30대"
+        case .forties: return "40대"
+        case .fifties: return "50대"
+        case .overThanSixty: return "60대 이상"
+        }
+    }
+    
+}
+
