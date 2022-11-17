4dd9ba32b54fb7c715c653285aa8f1759b9ef718
skycat0212 <mrmagnet777@gmail.com>
2022-11-18 00:51:53 +0900

    [Del] 중복 코드 삭제 (#35)

--- GetARock/GetARock/Network/Model/Band.swift
+++ GetARock/GetARock/Network/Model/Band.swift
@@ -22,25 +22,6 @@ struct Band {
     var introduction: String
 }
 
-enum PlayPosition {
-    case vocal
-    case guitar
-    case keyboard
-    case drum
-    case bass
-    case etc
-    
-    func toKorean() -> String {
-        switch self {
-        case .vocal: return "보컬"
-        case .guitar: return "기타"
-        case .keyboard: return "키보드"
-        case .drum: return "드럼"
-        case .bass: return "베이스"
-        case .etc: return "그 외"
-        }
-    }
-}
 
 enum AgeGroup {
     case lessThanTwenty
