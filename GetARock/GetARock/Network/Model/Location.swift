4eb5db05a656c3300fa548139ed8a864cb50c3cd
skycat0212 <mrmagnet777@gmail.com>
2022-11-18 17:16:16 +0900

    [Add] 장소정보 정의(#35)

--- GetARock/GetARock/Network/Model/Location.swift
+++ GetARock/GetARock/Network/Model/Location.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 struct Location {
-    
+    /// 나타내고자하는  위치의 이름입니다.
+    let name: String
+    /// 나타내고자하는  위치의 주소입니다.
+    let address: String
+    /// 나타내고자하는  위치의 세부 주소입니다.
+    /// ex) 건물의 동호수
+    let additionalAddress: String
+    /// 나타내고자하는  위치의 좌표입니다.
+    let coordinate: Coordinate
 }
