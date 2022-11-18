56a74e2b4500c227e231e3092f5166e4c98792bd
skycat0212 <mrmagnet777@gmail.com>
2022-11-18 16:37:30 +0900

    [Add] 좌표 모델 정의 (#35)

---
+++ GetARock/GetARock/Network/Model/Coordinate.swift
@@ -0,0 +1,15 @@
+//
+//  Coordinate.swift
+//  GetARock
+//
+//  Created by 김수진 on 2022/11/18.
+//
+
+import Foundation
+
+struct Coordinate {
+    typealias LocationDegrees = Double
+    
+    var latitude: LocationDegrees
+    var longitude: LocationDegrees
+}
