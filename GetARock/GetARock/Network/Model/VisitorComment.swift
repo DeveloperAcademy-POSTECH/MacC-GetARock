07a8bda468c3a4b904529cf679b47da054be2316
skycat0212 <mrmagnet777@gmail.com>
2022-11-19 16:01:19 +0900

    [Add] 방명록 정보 모델링 (#35)

---
+++ GetARock/GetARock/Network/Model/VisitorComment.swift
@@ -0,0 +1,15 @@
+//
+//  VisitorComment.swift
+//  GetARock
+//
+//  Created by 김수진 on 2022/11/18.
+//
+
+import Foundation
+
+struct VisitorComment {
+    let hostBand: BandInfo
+    let guestBand: BandInfo
+    let content: String
+    let createdAt: Date
+}
