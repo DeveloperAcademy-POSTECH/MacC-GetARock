701c6cedb813e9b1b395f67abe5d61bea05d7e01
GODNOEL <ah2650@naver.com>
2022-11-19 22:40:42 +0900

    [Add] UITableView Delegate, DataSource 추가 (#44)

--- GetARock/GetARock/Screen/BandInfo/Component/GuestBookListUIView.swift
+++ GetARock/GetARock/Screen/BandInfo/Component/GuestBookListUIView.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class GuestBookListUIView: UIView {
+    
+    private let reuseIdentifier = "GusetBookTableCell"
+    
+    private let tableView = UITableView()
 
+    // MARK: Init
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
+    
+    func setGuestBookList() {
+        self.addSubview(tableView)
+    }
+    
+}
+
+// MARK: - UITableViewDelegate
+extension GuestBookListUIView: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDataSource
+extension GuestBookListUIView: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return 10
+     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    }
+    
+
 }
