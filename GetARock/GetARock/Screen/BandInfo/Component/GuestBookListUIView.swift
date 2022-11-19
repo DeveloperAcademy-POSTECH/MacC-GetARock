ed820dbc31845def0d0d247c5fb68e98e72bf36d
GODNOEL <ah2650@naver.com>
2022-11-19 22:12:20 +0900

    [Add] GuestBookListUIView 파일 생성 (#44)

---
+++ GetARock/GetARock/Screen/BandInfo/Component/GuestBookListUIView.swift
@@ -0,0 +1,19 @@
+//
+//  GuestBookUIView.swift
+//  GetARock
+//
+//  Created by Yu ahyeon on 2022/11/19.
+//
+
+import UIKit
+
+class GuestBookListUIView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
