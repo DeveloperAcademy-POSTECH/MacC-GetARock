225442a31a1d651861c9b1a0d0c983feace3bb2a
skycat0212 <mrmagnet777@gmail.com>
2022-11-20 10:04:46 +0900

    [Add] 밴드가 포지션별 인원 정보를 갖도록 구현 (#35)

--- GetARock/GetARock/Network/Model/Band.swift
+++ GetARock/GetARock/Network/Model/Band.swift
@@ -13,10 +13,16 @@ struct BandInfo {
 }
 
 struct Band {
+    /// 포지션과 해당 포지션의 인원을 나타냅니다.
+    struct PositionSet {
+        var position: PlayPosition
+        var numberOfPerson: Int
+    }
+    
     /// 밴드의 이름입니다.
     var name: String
     /// 밴드가 갖고있는 포지션입니다.
-    var filledPosition: [PlayPosition]
+    var filledPosition: [PositionSet]
     /// 밴드의 합주곡입니다.
     var repertoire: [String]
     /// 밴드의 연령대입니다.
