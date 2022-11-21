8abc29ea6ca50927db6155f3de1323878c4379ba
Tanny <xogus9641@naver.com>
2022-11-21 22:40:17 +0900

    [Add] BandTimeline ViewController 생성 및 TableView 추가 (#70)

---
+++ GetARock/GetARock/Screen/BandInfo/BandTimelineViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BandTimelineViewController.swift
+//  GetARock
+//
+//  Created by Admin on 2022/11/21.
+//
+
+import UIKit
+
+class BandTimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
