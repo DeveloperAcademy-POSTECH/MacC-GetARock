ad982b144df26df7689da68d8ac91ff525d7a654
GODNOEL <ah2650@naver.com>
2022-11-21 14:28:25 +0900

    [Add] 댓글 버튼 UI 컴포넌트 생성 (#44)

---
+++ GetARock/GetARock/Global/UIComponent/CommentCreateButton.swift
@@ -0,0 +1,64 @@
+//
+//  CommentCreateButton.swift
+//  GetARock
+//
+//  Created by Yu ahyeon on 2022/11/20.
+//
+
+import UIKit
+
+class CommentCreateButton: UIView {
+
+    // MARK: - Property
+
+    private enum Size {
+        static let spacing: CGFloat = 16.0
+        static let height: CGFloat = 50.0
+        static let width: CGFloat = UIScreen.main.bounds.size.width - Size.spacing * 2
+    }
+
+    // MARK: - View
+
+    lazy var buttonTitle: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIButton())
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        attribute()
+        setupLayout()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    // MARK: - Method
+
+    private func attribute() {
+        self.backgroundColor = .appColor(.mainPurple)
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = 10
+        NSLayoutConstraint.activate([
+            self.centerXAnchor.constraint(equalTo: centerXAnchor),
+            self.centerYAnchor.constraint(equalTo: centerYAnchor),
+            self.widthAnchor.constraint(equalToConstant: Size.width),
+            self.heightAnchor.constraint(equalToConstant: Size.height)
+        ])
+    }
+
+    private func setupLayout() {
+        self.addSubview(buttonTitle)
+        NSLayoutConstraint.activate([
+            buttonTitle.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            buttonTitle.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+    }
+
+    func setupButtonTitle(title: String) {
+        buttonTitle.setTitle(title, for: .normal)
+    }
+}
