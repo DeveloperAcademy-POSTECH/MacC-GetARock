584ab0c834f2690f4c978287661df343afb4168a
siro96-01 <youjin22@pos.idserve.net>
2022-11-22 10:38:42 +0900

    코드리뷰 수정점 반영:conditionView로 이름변경, Mypage 삭제

--- GetARock/GetARock/Screen/GatheringInfo/GatheringInfoViewController.swift
+++ GetARock/GetARock/Screen/GatheringInfo/GatheringInfoViewController.swift
@@ -9,26 +9,17 @@ import UIKit
 
 class GatheringInfoViewController: UIViewController {
     
+    //MARK: - properties
     @IBOutlet weak var conditionContainer: UIView!
     @IBOutlet weak var introductionContentView: UIView!
     
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         conditionContainer.layer.cornerRadius = 15
         conditionContainer.layer.borderWidth = 2
         conditionContainer.layer.borderColor = UIColor.white.cgColor
-        introductionContentView.layer.cornerRadius = 15
         
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
