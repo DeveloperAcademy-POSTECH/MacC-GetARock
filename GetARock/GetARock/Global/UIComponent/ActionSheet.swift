c5cfdaa6a1b616fba975d5bfef28f2a1bca5dbe1
siro96-01 <youjin22@pos.idserve.net>
2022-11-22 15:08:32 +0900

    신고/삭제 ActionSheet 구현

---
+++ GetARock/GetARock/Global/UIComponent/ActionSheet.swift
@@ -0,0 +1,34 @@
+//
+//  ActionSheet.swift
+//  GetARock
+//
+//  Created by 진영재 on 2022/11/22.
+//
+
+import UIKit
+
+//class ActionSheet {
+    
+//    func showActionSheet() {
+//        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+//        
+//        let cancel = UIAlertAction(title: "취소", style: .cancel) { action in
+//            
+//        }
+//        let delete = UIAlertAction(title: "삭제하기", style: .destructive) { action in
+//            print("delete")
+//        }
+//        let report = UIAlertAction(title: "신고하기", style: .default) { action in
+//            print("report")
+//        }
+//        
+//        actionSheet.addAction(report)
+//        actionSheet.addAction(delete)
+//        actionSheet.addAction(cancel)
+//        
+//    }
+////}
+//
+//class AlertSheet {
+//    
+//}
--- GetARock/GetARock/Screen/GatheringInfo/GatheringInfoViewController.swift
+++ GetARock/GetARock/Screen/GatheringInfo/GatheringInfoViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class GatheringInfoViewController: UIViewController {
     
-    //MARK: - properties
+    // MARK: - properties
     @IBOutlet weak var conditionContainer: UIView!
     @IBOutlet weak var introductionContentView: UIView!
+    
+    @IBOutlet weak var ellipsis: UIButton!
+    //    var actionSheet = ActionSheet()
+
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
@@ -21,5 +25,31 @@ class GatheringInfoViewController: UIViewController {
         conditionContainer.layer.borderColor = UIColor.white.cgColor
         
     }
+   
+    @IBAction func ellipsis(_ sender: UIButton) {
+        showActionSheet()
+    }
     
+
+//        actionSheet.showActionSheet()
+
+    func showActionSheet() {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel) { action in
+            
+        }
+        let delete = UIAlertAction(title: "삭제하기", style: .destructive) { action in
+            print("delete")
+        }
+        let report = UIAlertAction(title: "신고하기", style: .default) { action in
+            print("report")
+        }
+        
+        actionSheet.addAction(report)
+        actionSheet.addAction(delete)
+        actionSheet.addAction(cancel)
+        
+        present(actionSheet, animated: true)
+    }
 }
