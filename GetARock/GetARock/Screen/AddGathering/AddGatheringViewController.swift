bd25731880265f16472ccc5f573c34938bb4fe61
EuniceNam <caellime@gmail.com>
2022-11-22 14:59:43 +0900

    [Add] TextView의 Placeholder 추가 (#36)

--- GetARock/GetARock/Screen/AddGathering/AddGatheringViewController.swift
+++ GetARock/GetARock/Screen/AddGathering/AddGatheringViewController.swift
@@ -9,17 +9,54 @@ import UIKit
 
 class AddGatheringViewController: UIViewController {
 
+    // MARK: - View
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var hostBandNameLabel: UILabel!
+    @IBOutlet weak var aboutTextView: UITextView!
+
+    private let placeHolderLabel: UILabel = {
+        $0.text = "내용을 입력하세요"
+        $0.textColor = .lightGray
+        $0.sizeToFit()
+        $0.font = .preferredFont(forTextStyle: .subheadline)
+        return $0
+    }(UILabel())
+
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupNavigationBar()
+        attribute()
+        setupLayout()
     }
 
     // MARK: - Method
 
+    private func attribute() {
+        setupNavigationBar()
+    }
+
     private func setupNavigationBar() {
         navigationController?.navigationBar.shadowImage = UIImage()
+    }
+
+    private func setupLayout() {
+        view.addSubview(placeHolderLabel)
+        placeHolderLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            placeHolderLabel.topAnchor.constraint(equalTo: aboutTextView.topAnchor, constant: 8),
+            placeHolderLabel.leadingAnchor.constraint(equalTo: aboutTextView.leadingAnchor, constant: 4)
+        ])
+        aboutTextView.delegate = self
+    }
+}
+
+// MARK: - UITextViewDelegate
+
+extension AddGatheringViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        self.placeHolderLabel.textColor = aboutTextView.text.count == 0 ? .lightGray : .clear
     }
 }
