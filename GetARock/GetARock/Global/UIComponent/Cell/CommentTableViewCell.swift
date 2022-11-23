a360c7edde78d4c2d26a3cd7768c9e0cf9227e95
GODNOEL <ah2650@naver.com>
2022-11-23 16:07:06 +0900

    [Add] 불필요한 코드 삭제 (#84)

--- GetARock/GetARock/Global/UIComponent/Cell/CommentTableViewCell.swift
+++ GetARock/GetARock/Global/UIComponent/Cell/CommentTableViewCell.swift
@@ -11,46 +11,43 @@ class CommentTableViewCell: UITableViewCell {
     
     // MARK: - View
 
-    lazy var bandName: UILabel = {
-        $0.text = "노엘천재"
+    let bandName: UILabel = {
         $0.textColor = .white
         $0.font = UIFont.systemFont(ofSize: 16, weight: .bold)
         return $0
     }(UILabel())
 
-    lazy var moreButton: UIButton = {
+    let moreButton: UIButton = {
         $0.setImage(UIImage(systemName: "ellipsis"), for: .normal)
         $0.tintColor = .white
         return $0
     }(UIButton())
 
-    lazy var commentText: UILabel = {
-        $0.text = "노엘천재(본문)노엘천재(본문)노엘천재(본문)노엘천재(본문)노엘천재(본문)노엘천재(본문)노엘천재(본문)노엘천재(본문)노엘천재(본문)노엘천재(본문)노엘천재(본문)노엘천재(본문)노엘천재(본문)노엘천재(본문)"
+    let commentText: UILabel = {
         $0.numberOfLines = 0
         $0.textColor = .white
         $0.font = UIFont.systemFont(ofSize: 14)
         return $0
     }(UILabel())
 
-    lazy var commentDate: UILabel = {
-        $0.text = "2022.11.20"
+    let commentDate: UILabel = {
         $0.textColor = .lightGrey
         $0.font = UIFont.systemFont(ofSize: 12)
         return $0
     }(UILabel())
 
-    lazy var commentInfo: UIStackView = {
+    lazy var commentInfoStackView: UIStackView = {
         $0.axis = .horizontal
         $0.distribution = .equalSpacing
         return $0
     }(UIStackView(arrangedSubviews: [bandName, moreButton]))
 
-    lazy var comment: UIStackView = {
+    lazy var commentStackView: UIStackView = {
         $0.axis = .vertical
         $0.spacing = 10
         $0.translatesAutoresizingMaskIntoConstraints = false
         return $0
-    }(UIStackView(arrangedSubviews: [commentInfo, commentText, commentDate]))
+    }(UIStackView(arrangedSubviews: [commentInfoStackView, commentText, commentDate]))
 
     // MARK: - Init
 
@@ -66,12 +63,12 @@ class CommentTableViewCell: UITableViewCell {
     // MARK: - Method
 
     private func setupLayout() {
-        self.contentView.addSubview(comment)
+        self.contentView.addSubview(commentStackView)
         NSLayoutConstraint.activate([
-            comment.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 30),
-            comment.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 36),
-            comment.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -36),
-            comment.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -30)
+            commentStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 30),
+            commentStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 36),
+            commentStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -36),
+            commentStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -30)
         ])
     }
     
