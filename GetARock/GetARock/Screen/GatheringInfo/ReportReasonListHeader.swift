ee2b4826840c074cff3a02cb8c31f9a0af60d9a0
siro96-01 <youjin22@pos.idserve.net>
2022-11-23 10:39:12 +0900

    변경사항 임시저장

---
+++ GetARock/GetARock/Screen/GatheringInfo/ReportReasonListHeader.swift
@@ -0,0 +1,13 @@
+//
+//  ReportReasonListHeader.swift
+//  GetARock
+//
+//  Created by 진영재 on 2022/11/23.
+//
+
+import UIKit
+
+class ReportReasonListHeader: UITableViewHeaderFooterView {
+    
+   
+}
