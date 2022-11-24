b913d48fe5933e3d6442a39a5efda108c12cd527
Tanny <xogus9641@naver.com>
2022-11-24 13:13:09 +0900

    [Feat] 모달에서 데이터를 View에 넘겨주면 받을 수 있도록 목데이터에 맞춰서 구현 (#108)

--- GetARock/GetARock/Screen/BandInfo/VC/BandTimelineViewController.swift
+++ GetARock/GetARock/Screen/BandInfo/VC/BandTimelineViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class BandTimelineViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
+    var gatheringInfos: [GatheringInfo] = []
+    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
@@ -24,27 +26,28 @@ class BandTimelineViewController: UIViewController {
 
 extension BandTimelineViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return gathering.count
-        return 1
+        return gatheringInfos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: BandTimelineCell.className, for: indexPath) as? BandTimelineCell else { return UITableViewCell() }
         
-//        cell.title.text = gathering[indexPath.row].title
-//        cell.startTime.text = gathering[indexPath.row].date
-//        let labelType = gathering[indexPath.row].status
-//        if labelType == .recruiting || labelType == .progressing {
-//            cell.status.text = labelType.toKorean()
-//        } else {
-//            cell.status.isHidden = true
-//        }
-//
-//        if indexPath.row == 0 {
-//            cell.topLine.isHidden = true
-//        } else if indexPath.row == gathering.count - 1 {
-//            cell.bottomLine.isHidden = true
-//        }
+        let gathering = gatheringInfos[indexPath.row].gathering
+        cell.title.text = gathering.title
+        cell.startTime.text = gathering.date.toString(format: DateFormatLiteral.standard)
+        
+        let statusType = gathering.status
+        if statusType == .recruiting || statusType == .progressing {
+            cell.status.text = statusType.toKorean()
+        } else {
+            cell.status.isHidden = true
+        }
+
+        if indexPath.row == 0 {
+            cell.topLine.isHidden = true
+        } else if indexPath.row == (gatheringInfos.count) - 1 {
+            cell.bottomLine.isHidden = true
+        }
         
         return cell
     }
