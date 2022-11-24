dadd0899d7b4c30a1fd8dc734f006312b1744801
Tanny <xogus9641@naver.com>
2022-11-24 11:22:06 +0900

    [Del] 임시로 생성한 데이터 삭제 (#107)

--- GetARock/GetARock/Screen/Gathering/GatheringListViewController.swift
+++ GetARock/GetARock/Screen/Gathering/GatheringListViewController.swift
@@ -13,47 +13,6 @@ class GatheringListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    // MARK: - Sample Data
-    
-    enum StateType {
-        case finding
-        case active
-        case finish
-        case cancel
-    }
-    
-    struct Gathering {
-        var title: String
-        var date: String
-        var state: StateType
-        
-        init(title: String, date: String, state: StateType) {
-            self.title = title
-            self.date = date
-            self.state = state
-        }
-    }
-    
-    private var gathering = [
-        Gathering(title: "동성로 합동 버스킹 합니다!", date: "22.12.05 13:00", state: .finding),
-        Gathering(title: "블랙로즈 합주 드럼 한 분 구합니다", date: "22.11.20 14:00", state: .active),
-        Gathering(title: "몰라 아무거나 쳐보는 중입니다.", date: "22.11.20 12:00", state: .active),
-        Gathering(title: "두 줄 만들기 해보는 중, 몰라 아무거나 쳐보는 중입니다1234.", date: "22.11.18 21:00", state: .cancel),
-        Gathering(title: "대체텍스트 대체텍스트 대체텍스트", date: "22.11.18 16:00", state: .finish),
-        Gathering(title: "두 줄 만들기 대체텍스트 대체텍스트 대체텍스트 대체텍스트 대체텍스트 22", date: "22.11.12 18:00", state: .finish)
-    ]
-    
-    func labelType(for type: StateType) -> String {
-        let text: String
-        switch type {
-        case .finding: text = "모집중"
-        case .active: text = "진행중"
-        case .finish: text = "완 료"
-        case .cancel: text = "취소됨"
-        }
-        return text
-    }
-    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
@@ -69,15 +28,16 @@ class GatheringListViewController: UIViewController {
 
 extension GatheringListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return gathering.count
+//        return gathering.count
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: GatheringListCell.className, for: indexPath) as? GatheringListCell else { return UITableViewCell() }
         
-        cell.title.text = gathering[indexPath.row].title
-        cell.startTime.text = gathering[indexPath.row].date
-        cell.state.text = labelType(for: gathering[indexPath.row].state)
+//        cell.title.text = gathering[indexPath.row].title
+//        cell.startTime.text = gathering[indexPath.row].date
+//        cell.state.text = labelType(for: gathering[indexPath.row].state)
         
         return cell
     }
