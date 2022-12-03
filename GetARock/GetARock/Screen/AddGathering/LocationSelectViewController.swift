cac2c846a7919e9f77cda9840256fb4555da1af2
EuniceNam <caellime@gmail.com>
2022-12-03 21:18:42 +0900

    [Feat] 장소 검색에서 일부 주소 입력시 자동완성 결과 테이블뷰에 나옴 (#38)
    선택하기는 아직임

--- GetARock/GetARock/Screen/AddGathering/LocationSelectViewController.swift
+++ GetARock/GetARock/Screen/AddGathering/LocationSelectViewController.swift
@@ -12,15 +12,12 @@ class LocationSelectViewController: UIViewController {
 
     // MARK: - Property
 
-    var placeResults: [MKLocalSearchCompletion] = []
-//    {
-//        didSet {
-//            print("didSet placeResults count: \(placeResults.count)")
-//            tableView.reloadData()
-//        }
-//    }
-
     var searchCompleter: MKLocalSearchCompleter?
+    var placeResults: [MKLocalSearchCompletion] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
 
     // MARK: - View
 
@@ -30,14 +27,44 @@ class LocationSelectViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let nibName = UINib(nibName: "LocationCandidateCell", bundle: nil)
+        let nibName = UINib(nibName: "LocationCandidateCell", bundle: nil) //.className이 잘 작동 안해서 일단 이렇게 쓰는 중
         tableView.register(nibName, forCellReuseIdentifier: "LocationCandidateCell")
         tableView.dataSource = self
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        startProvidingCompletions()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        stopProvidingCompletions()
+    }
+
+    // MARK: - Method
+
+    private func startProvidingCompletions() {
+        searchCompleter = MKLocalSearchCompleter()
+        searchCompleter?.delegate = self
+        // MKCoordinateRegion 필요시 설정. 한국 등은 여기 말고 placemark의 countryCode에서 처리
+        // searchCompleter?.region = searchRegion
+    }
+    
+    private func stopProvidingCompletions() {
+        searchCompleter = nil
+    }
 }
 
-// MARK: - Search Results Updating
+// MARK: - MK Local Search Completer Delegate (지역검색 결과 업데이트)
+
+extension LocationSelectViewController: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        placeResults = completer.results
+    }
+}
+
+// MARK: - Search Results Updating (search controller 관련)
 
 extension LocationSelectViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
@@ -53,9 +80,12 @@ extension LocationSelectViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCandidateCell", for: indexPath) as? LocationCandidateCell else { return UITableViewCell() }
-//        cell.titleLabel?.text = placeResults[(indexPath as NSIndexPath).row].title
-//        cell.addressLabel?.text = placeResults[(indexPath as NSIndexPath).row].subtitle
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCandidateCell", for: indexPath) as?
+                LocationCandidateCell else {
+            return UITableViewCell()
+        }
+        cell.addressNameLabel?.text = placeResults[(indexPath as NSIndexPath).row].title
+        cell.addressLabel?.text = placeResults[(indexPath as NSIndexPath).row].subtitle
 
         return cell
     }
