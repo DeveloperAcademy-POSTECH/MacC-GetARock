be9a4110a81db78662f13d74a988555c9224ee40
skycat0212 <mrmagnet777@gmail.com>
2022-12-04 18:52:20 +0900

    [Add] VisitorCommentDTO 정의 (#119)

--- GetARock/GetARock/Network/Model/DTO/VisitorCommentDTO.swift
+++ GetARock/GetARock/Network/Model/DTO/VisitorCommentDTO.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+import Firebase
+
+struct VisitorCommentDTO: Codable {
+    let hostBandID: String
+    var authorID: String
+    let content: String
+    var createdAt: Timestamp
+}
+
+extension VisitorCommentDTO {
+    
+    func toVisitorComment() async throws -> VisitorComment {
+        let bandAPI = BandAPI()
+        
+        async let hostBand = bandAPI.getBandInfo(bandID: self.hostBandID)
+        async let auther = bandAPI.getBandInfo(bandID: self.authorID)
+        
+        return VisitorComment(
+            hostBand: try await hostBand,
+            author: try await auther,
+            content: self.content,
+            createdAt: self.createdAt.dateValue()
+        )
+        
+    }
+    
+}
+
+extension VisitorComment {
+    
+    func toVisitorCommentDTO() -> VisitorCommentDTO {
+        return VisitorCommentDTO(
+            hostBandID: self.hostBand.bandID,
+            authorID: self.author.bandID,
+            content: self.content,
+            createdAt: Timestamp(date: self.createdAt)
+        )
+    }
+    
+}
