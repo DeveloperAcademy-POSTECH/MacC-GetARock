1e4622ca7abf35fa8bc898d02b21d8c1948556b6
siro96-01 <youjin22@pos.idserve.net>
2023-01-06 00:13:18 +0900

    [Fix] GatheringInfo가 연결되지 않는 버그 수정 (#126))

--- GetARock/GetARock/Screen/GatheringInfo/GatheringInfoViewController.swift
+++ GetARock/GetARock/Screen/GatheringInfo/GatheringInfoViewController.swift
@@ -2,41 +2,40 @@
 //  GatheringInfoViewController.swift
 //  GetARock
 //
-//  Created by 진영재 on 2022/11/21.
+//  Created by 진영재 on 2023/01/05.
 //
 
-import UIKit
+        import UIKit
 
-final class GatheringInfoViewController: UIViewController {
-    
-    // MARK: - properties
-    
-    @IBOutlet weak var conditionView: UIView!
-    @IBOutlet weak var introductionContentView: UIView!
-    @IBOutlet weak var ellipsis: UIButton!
-    
-    // MARK: - View Life Cycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        conditionView.layer.cornerRadius = 15
-        conditionView.layer.borderWidth = 2
-        conditionView.layer.borderColor = UIColor.white.cgColor
-        
-    }
-    
-    // MARK: - Method
-    
-    @IBAction func touchUpInsideEllipsis(_ sender: UIButton) {
-        showActionSheet()
-    }
-    
-}
+        final class GatheringInfoViewController: UIViewController {
+            
+            // MARK: - properties
+            
+            @IBOutlet weak var conditionView: UIView!
+            @IBOutlet weak var ellipsisButton: UIButton!
+            
+            // MARK: - View Life Cycle
+            
+            override func viewDidLoad() {
+                super.viewDidLoad()
+                conditionView.layer.cornerRadius = 15
+                conditionView.layer.borderWidth = 2
+                conditionView.layer.borderColor = UIColor.white.cgColor
+                
+            }
+            
+            // MARK: - Method
+            
+            @IBAction func touchUpInsideEllipsis(_ sender: UIButton) {
+                showActionSheet()
+            }
+            
+        }
 
-// MARK: - extension Reportable Method
+        // MARK: - extension Reportable Method
 
-extension GatheringInfoViewController: Reportable {
-    func alertActionButtonPressed() {
-        print("삭제에 성공했습니다.")
-    }
-}
+        extension GatheringInfoViewController: Reportable {
+            func alertActionButtonPressed() {
+                print("삭제에 성공했습니다.")
+            }
+        }
