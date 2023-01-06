0530edbb5ccbdc901613c4a16774141764d00842
siro96-01 <youjin22@pos.idserve.net>
2023-01-06 23:16:06 +0900

    [Style] 유니스 코드리뷰 반영 (#125)

--- GetARock/GetARock/Screen/GatheringInfo/GatheringInfoViewController.swift
+++ GetARock/GetARock/Screen/GatheringInfo/GatheringInfoViewController.swift
@@ -5,40 +5,40 @@
 //  Created by 진영재 on 2023/01/05.
 //
 
-        import UIKit
+import UIKit
 
-        final class GatheringInfoViewController: UIViewController {
-            
-            // MARK: - properties
-            
-            @IBOutlet weak var conditionView: UIView!
-            @IBOutlet weak var statusLabel: UILabel!
-            @IBOutlet weak var ellipsisButton: UIButton!
-            @IBOutlet weak var gatheringTitleLabel: UILabel!
-            @IBOutlet weak var bandNameLabel: UILabel!
-            @IBOutlet weak var gatheringDateLabel: UILabel!
-            @IBOutlet weak var gatheringLocationLabel: UILabel!
-            @IBOutlet weak var describtionLabel: UILabel!
-            @IBOutlet weak var commentsView: UIView!
+final class GatheringInfoViewController: UIViewController {
+    
+    // MARK: - properties
+    
+    @IBOutlet weak var statusView: UIView!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var ellipsisButton: UIButton!
+    @IBOutlet weak var gatheringTitleLabel: UILabel!
+    @IBOutlet weak var bandNameLabel: UILabel!
+    @IBOutlet weak var gatheringDateLabel: UILabel!
+    @IBOutlet weak var gatheringLocationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var commentsView: UIView!
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Method
+    
+    @IBAction func touchUpInsideEllipsis(_ sender: UIButton) {
+        showActionSheet()
+    }
+    
+}
 
-            // MARK: - View Life Cycle
-            
-            override func viewDidLoad() {
-                super.viewDidLoad()
-            }
-            
-            // MARK: - Method
-            
-            @IBAction func touchUpInsideEllipsis(_ sender: UIButton) {
-                showActionSheet()
-            }
-            
-        }
+// MARK: - extension Reportable Method
 
-        // MARK: - extension Reportable Method
-
-        extension GatheringInfoViewController: Reportable {
-            func alertActionButtonPressed() {
-                print("삭제에 성공했습니다.")
-            }
-        }
+extension GatheringInfoViewController: Reportable {
+    func alertActionButtonPressed() {
+        print("삭제에 성공했습니다.")
+    }
+}
