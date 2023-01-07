bbb6feb28a622553698acf23d9fa4b23be8e3bfa
compuTasha <mijookim.tasha@gmail.com>
2023-01-07 21:39:04 +0900

    [Del] 불필요한 변수 삭제 (#104)

--- GetARock/GetARock/Screen/MainMap/MainMapViewController.swift
+++ GetARock/GetARock/Screen/MainMap/MainMapViewController.swift
@@ -203,8 +203,7 @@ extension MainMapViewController: MKMapViewDelegate {
         UIGraphicsBeginImageContext(size)
         
         pinImage?.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-        annotationView?.image = resizedImage
+        annotationView?.image = UIGraphicsGetImageFromCurrentImageContext()
         
         return annotationView
         
