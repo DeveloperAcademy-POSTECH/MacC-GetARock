04db23657ad1551754483f8cb2f5820649a9e95d
EuniceNam <caellime@gmail.com>
2023-01-13 01:00:41 +0900

    [Fix] 주소가 없는 "주변 검색"을 결과에서 제외 (#124)

--- GetARock/GetARock/Screen/AddGathering/LocationSearchResultViewController.swift
+++ GetARock/GetARock/Screen/AddGathering/LocationSearchResultViewController.swift
@@ -72,7 +72,7 @@ class LocationSearchResultViewController: UIViewController {
 
 extension LocationSearchResultViewController: MKLocalSearchCompleterDelegate {
     func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
-        suggestedPlaces = completer.results
+        suggestedPlaces = completer.results.filter { $0.subtitle != "주변 검색" }
     }
 }
 
